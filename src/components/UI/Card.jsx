f778a3f3f570fc9af4a689c70b36a9cd227428c1
Hadiru Colley <dirudeen22@gmail.com>
2023-09-04 01:07:29 +0300

    modified the styling

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,7 +1,7 @@
 
 const Card = (props) => {
   // eslint-disable-next-line react/prop-types
-  return <main className=" bg-secondary-100 md:rounded-lg ">{props.children}</main>;
+  return <main className=" bg-secondary-100 md:rounded-[2rem] md:flex md:flex-row-reverse md:max-w-[50rem] md:p-5 md:gap-2 ">{props.children}</main>;
 };
 
 export default Card;
