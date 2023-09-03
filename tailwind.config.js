57386d349b5b9a4af5124623626e599b5dc2be3b
Hadiru Colley <dirudeen22@gmail.com>
2023-09-03 14:37:10 +0300

    Added custom colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        primary: { 400: "#ff6257" },
+      },
+    },
   },
   plugins: [],
 };
