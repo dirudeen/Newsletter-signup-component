ca6e32202d63620f9936dedf5dd1d1dbe5e1576e
Hadiru Colley <dirudeen22@gmail.com>
2023-09-03 21:13:16 +0300

    Added form validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,63 @@
-
+import { useCallback, useState } from "react";
 
 const Form = () => {
-    return (
-      <form>
-        <div className="grid gap-[0.5rem] mb-6" >
-          <label htmlFor="email" className="font-bold text-sm">Email address</label>
-          <input className="border border-slate-400 p-4 rounded-lg" type="email" id="email" placeholder="email@company.com" />
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  let isValid = enteredEmail.trim().includes("@");
+
+  const inputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+
+  const inputBlurHandler = useCallback(() => {
+    setIsTouched(true);
+  }, []);
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    if (!isValid) {
+      return;
+    }
+
+    setIsTouched(false);
+    setEnteredEmail("");
+  };
+
+  const errorClass = isTouched && !isValid 
+    ? " border-red-400 bg-red-100 placeholder:text-red-400"
+    : " border-slate-400 focus:border-slate-600 focus:border-[1.5px]";
+
+  return (
+    <form onSubmit={submitFormHandler}>
+      <div className=" mb-6">
+        <div className="flex justify-between mb-2">
+          <label htmlFor="email" className="font-bold text-sm">
+            Email address
+          </label>
+          {isTouched && !isValid && (
+            <p className="text-primary-400 text-sm font-bold">
+              Valid Email required
+            </p>
+          )}
         </div>
-        <button className="w-full text-center text-secondary-100 p-4 bg-secondary-800 rounded-lg">Subscribe to monthly newsletter</button>
-      </form>
-    );
-}
+        <input
+          className={`w-full outline-none rounded-lg p-4 border ${errorClass}`}
+          type="email"
+          id="email"
+          value={enteredEmail}
+          placeholder="email@company.com"
+          onBlur={inputBlurHandler}
+          onChange={inputChangeHandler}
+        />
+      </div>
+      <button className="w-full text-center text-secondary-100 p-4 bg-secondary-800 rounded-lg hover:bg-hoverClr ">
+        Subscribe to monthly newsletter
+      </button>
+    </form>
+  );
+};
 
 export default Form;
