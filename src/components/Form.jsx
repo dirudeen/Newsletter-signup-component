5ac45d99b24b1ca97d629eab8f2c9cac3cbc4f8b
Hadiru Colley <dirudeen22@gmail.com>
2023-09-03 19:42:30 +0300

    Added styles to the jsx code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,11 +3,11 @@
 const Form = () => {
     return (
       <form>
-        <div>
-          <label htmlFor="email">Email address</label>
-          <input type="email" id="email" placeholder="email@company.com" />
+        <div className="grid gap-[0.5rem] mb-6" >
+          <label htmlFor="email" className="font-bold text-sm">Email address</label>
+          <input className="border border-slate-400 p-4 rounded-lg" type="email" id="email" placeholder="email@company.com" />
         </div>
-        <button>Subscribe to monthly newsletter</button>
+        <button className="w-full text-center text-secondary-100 p-4 bg-secondary-800 rounded-lg">Subscribe to monthly newsletter</button>
       </form>
     );
 }
