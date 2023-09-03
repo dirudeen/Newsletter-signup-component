7ba12c2eab68aa9f1a56866e43c903b705443094
Hadiru Colley <dirudeen22@gmail.com>
2023-09-03 15:08:38 +0300

    Added content to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import Card from "./components/UI/Card";
+import Form from "./components/Form";
+import mobileImg from "./assets/images/illustration-sign-up-mobile.svg";
 function App() {
-
   return (
-    <>
-    <p>hello</p>
-    </>
-  )
+    <Card>
+      <section>
+        <h1>Stay updated!</h1>
+        <p>Join 60,000+ product managers receiving monthly updates on:</p>
+        <div>Product discovery and building what matters</div>
+        <div>Measuring to ensure updates are a success</div>
+        <div>And much more!</div>
+        <Form />
+      </section>
+
+      <section>
+        <div>
+          <img src={mobileImg} alt="sign up image" />
+        </div>
+      </section>
+    </Card>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,7 @@
+
+const Card = (props) => {
+  // eslint-disable-next-line react/prop-types
+  return <main className=" bg-secondary-100 rounded-lg ">{props.children}</main>;
+};
+
+export default Card;
