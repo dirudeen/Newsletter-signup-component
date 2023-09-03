9180e8714222c236327fa8fa737dc1c71509281d
Hadiru Colley <dirudeen22@gmail.com>
2023-09-04 00:06:35 +0300

    Added dismisMessageHandler function

--- src/components/SuccessfulSub.jsx
+++ src/components/SuccessfulSub.jsx
@@ -1,20 +1,32 @@
+import { useContext } from "react";
 import iconSuccess from "../assets/images/icon-success.svg";
+import Context from "../context/context";
+
 
 const SuccessfulSub = () => {
+  const { dispatch } = useContext(Context);
+const dismisMessageHandler = () => {
+  return dispatch({type: 'Done'})
+}
+
   return (
-    <section>
-      <div>
-        <img src={iconSuccess} alt="success icon" />
+    <section className="px-8 border border-slate-600 grid h-screen">
+      <div className="mt-[8rem]">
+        <div className="mb-6">
+          <img src={iconSuccess} alt="success icon" />
+        </div>
+        <h1 className="text-4xl font-bold mb-4">Thanks for subscribing! </h1>
+        <p className="mb-4">
+          A confirmation email has been sent to
+          <span className="font-bold"> ash@loremcompany.com</span>. Please open
+          it and click the button inside to confirm your subscription.
+        </p>
       </div>
-      <h1>Thanks for subscribing! </h1>
-
-      <p>
-        A confirmation email has been sent to
-        <span>ash@loremcompany.com</span>. Please open it and click the button
-        inside to confirm your subscription.
-      </p>
-
-      <button>Dismiss message</button>
+      <div className="flex items-end py-10">
+        <button className="text-center text-secondary-100 p-4 w-full bg-secondary-800 rounded-lg hover:bg-hoverClr" onClick={dismisMessageHandler}>
+          Dismiss message
+        </button>
+      </div>
     </section>
   );
 };
