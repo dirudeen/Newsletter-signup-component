a36b7635b11d318180bd708db140aa2483fa8206
Hadiru Colley <dirudeen22@gmail.com>
2023-09-04 00:03:59 +0300

    Added condional rendering using context state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Card from "./components/UI/Card";
-import Subsciption from "./components/Subsciption";
+import Subscription from "./components/Subscription";
+import SuccessfulSub from "./components/SuccessfulSub";
+import { useContext } from "react";
+import Context from "./context/context";
 
 function App() {
+  const {showSuccessPage} = useContext(Context)
+  console.log(showSuccessPage)
   return (
-    <Card>
-    <Subsciption />
-    </Card>
+      <Card>
+        {showSuccessPage ? <SuccessfulSub /> : <Subscription />}
+      </Card>
+  
   );
 }
 
