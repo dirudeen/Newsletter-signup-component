ebf3efb3c85b6aaf772391f99c7034011df4bc77
Hadiru Colley <dirudeen22@gmail.com>
2023-09-04 18:31:14 +0300

    added email state to the context

--- src/context/ShowPage-context.js
+++ src/context/ShowPage-context.js
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 
-const ShowPageContext = createContext({ showSuccessPage: false, dispatch: ()=>{}});
+const ShowPageContext = createContext({ showSuccessPage: false, dispatch: ()=>{}, email: ''});
 
 export default ShowPageContext;
--- src/context/ShowPage-provider.jsx
+++ src/context/ShowPage-provider.jsx
@@ -5,30 +5,37 @@ import { useReducer } from "react";
 function reducer(state, action) {
   if (action.type === "Success") {
     return {
-      ...state.showSuccessPage,
+      ...state,
       showSuccessPage: true,
+      email: action.enteredEmail,
     };
   }
 
   if (action.type === "Done") {
     return {
-      ...state.showSuccessPage,
+      ...state,
       showSuccessPage: false,
+      email: '',
     };
   }
 
   return {
     showSuccessPage: false,
+    email: '',
   };
 }
 
+const initialState = {
+  showSuccessPage: false,
+  email: ''
+};
+
+
 const ShowPageProvider = (props) => {
-  const [state, dispatch] = useReducer(reducer, {
-    showSuccessPage: false,
-  });
+  const [state, dispatch] = useReducer(reducer, initialState );
   return (
     <ShowPageContext.Provider
-      value={{ showSuccessPage: state.showSuccessPage, dispatch }}
+      value={{ showSuccessPage: state.showSuccessPage, dispatch, email: state.email }}
     >
       {props.children}
     </ShowPageContext.Provider>
