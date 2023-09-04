1b2bf99386dfec3002c4ddf426c21b0f1b652121
Hadiru Colley <dirudeen22@gmail.com>
2023-09-04 18:19:30 +0300

    Added styles for larger screens

--- src/components/SuccessfulSub.jsx
+++ src/components/SuccessfulSub.jsx
@@ -10,19 +10,19 @@ const dismisMessageHandler = () => {
 }
 
   return (
-    <section className="px-8 border border-slate-600 grid h-screen">
-      <div className="mt-[8rem]">
+    <section className="px-8 grid h-screen md:block md:h-auto md:p-8 md:w-[28rem]">
+      <div className="mt-[8rem] md:mt-0">
         <div className="mb-6">
           <img src={iconSuccess} alt="success icon" />
         </div>
-        <h1 className="text-4xl font-bold mb-4">Thanks for subscribing! </h1>
+        <h1 className="text-clamp leading-[1.1] font-bold mb-4">Thanks for subscribing! </h1>
         <p className="mb-4">
           A confirmation email has been sent to
           <span className="font-bold"> ash@loremcompany.com</span>. Please open
           it and click the button inside to confirm your subscription.
         </p>
       </div>
-      <div className="flex items-end py-10">
+      <div className="flex items-end py-10 md:pb-0">
         <button className="text-center text-secondary-100 p-4 w-full bg-secondary-800 rounded-lg hover:bg-hoverClr" onClick={dismisMessageHandler}>
           Dismiss message
         </button>
