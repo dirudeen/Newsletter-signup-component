2ab43c4da6cbb19a4e412144c4e2b869127bc331
Hadiru Colley <dirudeen22@gmail.com>
2023-09-04 18:32:03 +0300

    Used the email state context to output the email

--- src/components/SuccessfulSub.jsx
+++ src/components/SuccessfulSub.jsx
@@ -4,7 +4,7 @@ import ShowPageContext from "../context/ShowPage-context";
 
 
 const SuccessfulSub = () => {
-  const { dispatch } = useContext(ShowPageContext);
+  const { dispatch, email } = useContext(ShowPageContext);
 const dismisMessageHandler = () => {
   return dispatch({type: 'Done'})
 }
@@ -18,7 +18,7 @@ const dismisMessageHandler = () => {
         <h1 className="text-clamp leading-[1.1] font-bold mb-4">Thanks for subscribing! </h1>
         <p className="mb-4">
           A confirmation email has been sent to
-          <span className="font-bold"> ash@loremcompany.com</span>. Please open
+          <span className="font-bold"> {email}</span>. Please open
           it and click the button inside to confirm your subscription.
         </p>
       </div>
