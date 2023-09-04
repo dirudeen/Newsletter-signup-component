ee7a83b2dd74dcda6710ddfa5a19d75a0c86e3ea
Hadiru Colley <dirudeen22@gmail.com>
2023-09-04 14:19:44 +0300

    Added full height property

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,7 +1,7 @@
 
 const Card = (props) => {
   // eslint-disable-next-line react/prop-types
-  return <main className=" bg-secondary-100 md:rounded-[2rem] md:flex md:flex-row-reverse md:max-w-[50rem] md:p-5 md:gap-2 ">{props.children}</main>;
+  return <main className=" bg-secondary-100 md:rounded-[2rem] h-full md:h-auto md:flex md:flex-row-reverse md:gap-8 md:max-w-[55rem] md:p-5 " >{props.children}</main>;
 };
 
 export default Card;
